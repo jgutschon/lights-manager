5b220a7aa832aaf38b46a77672e5894cc55283e9
Jon Gutschon <jongutschon123@gmail.com>
2021-05-21 17:30:36 -0400

    child_process shell script running

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require("electron");
+const { exec, execFile } = require('child_process');
 const path = require("path");
 
 function createWindow() {
@@ -16,11 +17,25 @@ function createWindow() {
   // mainWindow.webContents.openDevTools()
 }
 
+function setupSerial() {
+  const child = execFile('bash', ['./shellScripts/findPort.sh'], (err, stdout, stderr) => {
+    if (err) {
+      console.error('stderr', stderr);
+      throw err;
+    }
+    const port = stdout;
+    exec(`stty 9600 -F ${port} raw -echo`);
+    exec(`echo -e 'f' > '${port}'`);
+  });
+}
+
+
 startApp = async () => {
   await app.whenReady();
   createWindow();
+  setupSerial();
 
-  app.on("activate", function () {
+  app.on("activate", () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
