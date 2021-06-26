1155372477518576cae8733f3da4eb1c1bc33a79
Jon Gutschon <jongutschon123@gmail.com>
2021-06-26 14:53:46 -0400

    consistent styling

--- main.js
+++ main.js
@@ -1,41 +1,40 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow } = require('electron');
 const { exec, execFile } = require('child_process');
-const path = require("path");
+const path = require('path');
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
+      preload: path.join(__dirname, 'preload.js'),
     },
   });
 
-  mainWindow.loadFile("index.html");
+  mainWindow.loadFile('index.html');
 
   // Open DevTools
-  // mainWindow.webContents.openDevTools()
-}
+  // mainWindow.webContents.openDevTools();
+};
 
-function setupSerial() {
-  const child = execFile('bash', ['./shellScripts/findPort.sh'], (err, stdout, stderr) => {
+setupSerial = async () => {
+  const child = execFile('bash', ['./shell/findPort.sh'], (err, stdout, stderr) => {
     if (err) {
       console.error('stderr', stderr);
       throw err;
     }
     const port = stdout;
     exec(`stty 9600 -F ${port} raw -echo`);
-    exec(`echo -e 'f' > '${port}'`);
+    exec(`cat ${port}`);
   });
-}
-
+};
 
 startApp = async () => {
   await app.whenReady();
   createWindow();
   setupSerial();
 
-  app.on("activate", () => {
+  app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
@@ -45,6 +44,6 @@ startApp = async () => {
 startApp();
 
 // Quit when all windows are closed, except on macOS
-app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") app.quit();
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
 });
