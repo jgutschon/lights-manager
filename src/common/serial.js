b50c6cc699808c60a8b70fbeb39cbb03ad1aa769
Jon Gutschon <jongutschon123@gmail.com>
2021-07-24 19:01:49 -0400

    fix asynchronous fileExec

--- src/common/serial.js
+++ src/common/serial.js
@@ -2,27 +2,28 @@ import { exec, execFile } from 'child_process';
 import path from 'path';
 
 const getSerialPort = () => {
-  const child = execFile(
-    path.resolve(__dirname, '../shell/findPort.sh'),
-    (err, stdout, stderr) => {
-      if (err) {
-        console.error('stderr', stderr);
-        throw err;
+  return new Promise((resolve, reject) => {
+    execFile(
+      path.resolve(__dirname, '../shell/findPort.sh'),
+      (err, stdout, stderr) => {
+        if (err) {
+          console.error('stderr', stderr);
+          return reject(err);
+        }
+
+        return resolve(stdout);
       }
-      return stdout;
-    }
-  );
+    );
+  });
 };
 
-const setupSerial = () => {
-  const port = getSerialPort();
+const setupSerial = async () => {
+  const port = await getSerialPort();
   exec(`stty 9600 -F ${port} raw -echo && cat ${port}`);
 };
 
-const sendMsg = (msg) => {
-  const port = getSerialPort();
-  console.log('port:', port);
-  console.log('msg:', msg);
+const sendMsg = async (msg) => {
+  const port = await getSerialPort();
   exec(`echo ${msg} > ${port}`);
 };
 
